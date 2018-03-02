6bb0ae4f5ecc9612d8ed43b628e55d264f84e39a
qlh <708937822@qq.com>
2018-03-02 12:08:47 +0800

    更新loading部分，增加提示语

--- app/src/main/java/cn/qlh/customview/MainActivity.java
+++ app/src/main/java/cn/qlh/customview/MainActivity.java
@@ -1,7 +1,10 @@
 package cn.qlh.customview;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.qlh.sdk.myview.loading.SpinKitViewManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+
+        SpinKitViewManager loading = new SpinKitViewManager(this);
+        loading.setProgressBarStyle(SpinKitViewManager.CUBE_GRID)
+                .setDlgBg(Color.parseColor("#18AD5C"))
+                .setTipsTxt("欢迎...")
+                .show();
+
     }
 }
--- myview/src/main/java/cn/qlh/sdk/myview/loading/SpinKitViewManager.java
+++ myview/src/main/java/cn/qlh/sdk/myview/loading/SpinKitViewManager.java
@@ -1,10 +1,12 @@
 package cn.qlh.sdk.myview.loading;
 
-import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.github.ybq.android.spinkit.SpriteFactory;
 import com.github.ybq.android.spinkit.Style;
@@ -23,6 +25,7 @@ public class SpinKitViewManager {
     private static int whichStyle = 0;
     private Dialog alertDialog;
     private View view;
+    private TextView tipsTv;
     //各种效果定义
     public static final int ROTATING_PLANE = 0;
     public static final int DOUBLE_BOUNCE = 1;
@@ -41,22 +44,47 @@ public class SpinKitViewManager {
     public static final int MULTIPLE_PULSE_RING = 14;
 
 
-    public SpinKitViewManager(Activity activity){
-        view = activity.getLayoutInflater().inflate(R.layout.spinkit_view_manager,null);
-        alertDialog = new Dialog(activity, R.style.customDialog);
+    public SpinKitViewManager(Context context){
+        view = LayoutInflater.from(context).inflate(R.layout.spinkit_view_manager,null);
+        alertDialog = new Dialog(context, R.style.customDialog);
         alertDialog.setCancelable(true);
         alertDialog.setCanceledOnTouchOutside(false);
         alertDialog.setContentView(view, new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
+                (int) context.getResources().getDimension(R.dimen.progress_ly_size),
+                (int) context.getResources().getDimension(R.dimen.progress_ly_size)));
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        tipsTv = (TextView) view.findViewById(R.id.tips_tv);
     }
 
     /**
      * 设置进度效果,如果不设置，默认ROTATING_PLANE
      * */
-    public void setProgressBarStyle(int intStyle){
+    public SpinKitViewManager setProgressBarStyle(int intStyle){
         whichStyle = intStyle;
+        return this;
+    }
+    /**
+     * 设置进度对话框背景颜色
+     * **/
+    public SpinKitViewManager setDlgBg(int color){
+
+        view.setBackgroundColor(color);
+        return this;
+    }
+    /**
+     * 设置提示语内容
+     * **/
+    public SpinKitViewManager setTipsTxt(String content){
+
+        tipsTv.setText(content);
+        return this;
+    }
+    /**
+     * 设置提示语颜色
+     * **/
+    public SpinKitViewManager setTipsColor(int color){
+        tipsTv.setTextColor(color);
+        return this;
     }
     public void show(){
         Style style = Style.values()[whichStyle];
